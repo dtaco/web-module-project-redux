bf10de8214b18c727667997a15059c0715fd638c
David Tacoronte <d.tacoronte89@gmail.com>
2023-09-23 13:45:56 -0400

    completed MVP functionality and stretch goal of removing favorite duplicates

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 
+import { deleteMovie } from '../actions/movieActions'
+import { addFavorite } from '../actions/favoritesActions'
+
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const { movies } = props;
+    const { movies, favorites, displayFavorites, deleteMovie , addFavorite } = props;
     const movie = movies.find(movie=>movie.id===Number(id));
     
+    const handleDeleteClick = () => {
+        deleteMovie(movie.id);
+        push('/movies');
+    }
+
+    const handleFavoriteClick = () => {
+        const exists = favorites.filter(item => (item.id === movie.id))
+        {exists.length === 0 && addFavorite(movie)}
+    }
+
     return(<div className="modal-page col">
         <div className="modal-dialog">
             <div className="modal-content">
@@ -38,8 +51,8 @@ const Movie = (props) => {
                         </section>
                         
                         <section>
-                            <span className="m-2 btn btn-dark">Favorite</span>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            {displayFavorites && <span onClick={handleFavoriteClick} className="m-2 btn btn-dark">Favorite</span>}
+                            <span className="delete" onClick={handleDeleteClick}><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
                 </div>
@@ -50,8 +63,10 @@ const Movie = (props) => {
 
 const mapStateToProps = (state) => {
     return({
-        movies:state.movies
+        displayFavorites: state.favoritesReducer.displayFavorites,
+        movies: state.movieReducer.movies,
+        favorites: state.favoritesReducer.favorites
     })
 }
 
-export default connect(mapStateToProps, {})(Movie);
+export default connect(mapStateToProps, {deleteMovie, addFavorite})(Movie);
